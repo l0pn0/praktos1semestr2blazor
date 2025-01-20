ea6d37a36094adfedda3d6ddafc7d0b7bf43243f
Денис Максимов <lopno00@outlook.com>
2025-01-20 09:22:18 +0300

    Добавьте файлы проекта.

---
+++ praktos1semestr2blazor/Model/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace praktos1semestr2blazor.Model
+{
+    public class LoginRequest
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ praktos1semestr2blazor/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace praktos1semestr2blazor.Model
+{
+    public class User
+    {
+        [Key]
+        public int id_User { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
+
